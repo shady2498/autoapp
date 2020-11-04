42075ea73ca28a67c80e488ae4550d3187747206
shady2498 <shahhassancs@gmail.com>
2020-11-04 16:24:52 +0500

    Home Page integrated without any styling

--- components/vehicles.js
+++ components/vehicles.js
@@ -10,7 +10,34 @@ export default function Vehicles(props) {
 						.slice(vehicles.length - props.show, vehicles.length)
 						.reverse()
 						.map((vehicles, index) => {
-							return <h1 key={index}>{vehicles.id}</h1>;
+							return (
+								<div className="actvitybox">
+									<div className="media">
+										<img
+											id="avatar1"
+											src={vehicles.image}
+											className="mr-3 mt-3 rounded-circle"
+											style={{ width: 60 }}
+										/>
+										<div className="media-body">
+											<h4>
+												<strong key={index}> {vehicles.CarOwner}</strong>{" "}
+											</h4>
+											<text>
+												current speed:
+												<strong key={index}>{vehicles.currentSpeed}</strong>
+											</text>
+											<text>
+												current Elevation:{" "}
+												<strong key={index}>
+													{" "}
+													{vehicles.currentElevation}{" "}
+												</strong>{" "}
+											</text>
+										</div>
+									</div>
+								</div>
+							);
 						})
 				: vehicles.reverse().map((vehicles, index) => {
 						return <h1 key={index}>{vehicles.id}</h1>;
