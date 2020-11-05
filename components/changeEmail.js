8809080cf8875e5e62c7112517af4d34a2d9aefc
shady2498 <shahhassancs@gmail.com>
2020-11-05 15:58:53 +0500

    worked on account editing

--- components/changeEmail.js
+++ components/changeEmail.js
@@ -1,29 +1,39 @@
-import { faVenusDouble } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 
 export default function changeEmail() {
-	const [value, handleChangeEmail] = useState({ email: "sdfsdfsf" });
+	const [value, handleChangeEmail] = useState({ name: "sdfsdfsf" });
 
-	const changeField = (event, value) => {
+	const changeField = (event) => {
 		event.preventDefault();
+		// console.log(event);
 		// var myJSON = JSON.stringify(value);
 		// console.log(myJSON);
-		console.log(value.target);
+		console.log(event.target.value);
 	};
+
+	const saveInput = (event) => {
+		console.log(event.target.value);
+	};
+
 	return (
-		<form onSubmit={(event) => changeField(event, value)}>
+		<form onSubmit={(event) => saveInput(event, value)}>
 			<div className="input-group">
 				<input
 					className="input--style-1"
-					type="name"
+					type="text"
 					name="name"
 					placeholder="Name"
-					value={value}
-					onChange={handleChangeEmail}
+					// onKeyPress={changeField}
+					// onChange={changeField}
+					onChange={changeField}
 				/>
 			</div>
 
-			<button type="submit" className="btn btn-primary btnsubmit">
+			<button
+				type="submit"
+				className="btn btn-primary btnsubmit"
+				// onClick={() => saveInput(value)}
+			>
 				<span className="submit"> Submit </span>
 			</button>
 		</form>
